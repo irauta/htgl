bf9d02087410d79db8ebefd311ab96c2ae444eac
Ilkka Rauta <ilkka.rauta@gmail.com>
2014-09-23 21:57:58 +0300

    Add quick sketch of convenience constructor for VAO, now commented.

--- src/vertexarray.rs
+++ src/vertexarray.rs
@@ -26,6 +26,8 @@ pub struct VertexAttribute {
     vertex_buffer: VertexBufferHandle
 }
 
+// pub type QuickVertexAttribute (u8, AttributeType, bool)
+
 pub struct VertexArray {
     vertex_attributes: Vec<VertexAttribute>,
     index_buffer: Option<IndexBufferHandle>
@@ -35,4 +37,9 @@ impl VertexArray {
     pub fn new(attributes: &[VertexAttribute], index_buffer: Option<IndexBufferHandle>) -> VertexArray {
         VertexArray { vertex_attributes: Vec::new(), index_buffer: index_buffer }
     }
+
+    /* pub fn new_single_vbo(attributes: &[QuickVertexAttribute],
+            vertex_buffer: VertexBufferHandle,
+            index_buffer: Option<IndexBufferHandle>) -> VertexArray {
+    } */
 }
